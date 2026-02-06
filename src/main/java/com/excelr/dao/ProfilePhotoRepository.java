732c7c6aa1fc271edd6f02d25ad5ecd04acd84bc
PNSVN3035 <154006068+PNSVN3035@users.noreply.github.com>
2026-02-06 02:11:20 -0600

    Delete src/main directory

--- src/main/java/com/excelr/dao/ProfilePhotoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.excelr.dao;
-
-import com.excelr.entity.ProfilePhoto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProfilePhotoRepository extends JpaRepository<ProfilePhoto, Integer> {
-}
